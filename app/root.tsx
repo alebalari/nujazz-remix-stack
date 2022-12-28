7fc2337452f0dd0e3e26b63e9cd36c3126c91b1e
Alejandro Balari <alejandro@deployblackbox.com>
2022-12-28 15:57:08 -0500

    move supabase context to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,17 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import type { LinksFunction, LoaderArgs, LoaderFunction, MetaFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
+import {
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useFetcher,
+	useLoaderData,
+} from '@remix-run/react';
+import { createBrowserClient, createServerClient } from '@supabase/auth-helpers-remix';
+import { useEffect, useState } from 'react';
 
 import tailwindStyles from './styles/tailwind.css';
 
@@ -11,7 +23,66 @@ export const meta: MetaFunction = () => ({
 	viewport: 'width=device-width,initial-scale=1',
 });
 
+export const loader: LoaderFunction = async ({ request, context }: LoaderArgs) => {
+	// Pipe any necessary environment variables to the client
+	// Cloudflare pages uses the `context` object to pass env vars
+	const env = {
+		SUPABASE_URL: context.SUPABASE_URL,
+		SUPABASE_ANON_PUBLIC_KEY: context.SUPABASE_ANON_PUBLIC_KEY,
+	};
+	// We can retrieve the session on the server and hand it to the client.
+	// This is used to make sure the session is available immediately upon rendering
+	const response = new Response();
+	const supabaseClient = createServerClient(env.SUPABASE_URL as string, env.SUPABASE_ANON_PUBLIC_KEY as string, {
+		request,
+		response,
+	});
+
+	const {
+		data: { session },
+	} = await supabaseClient.auth.getSession();
+
+	// in order for the set-cookie header to be set,
+	// headers must be returned as part of the loader response
+	return json(
+		{
+			env,
+			session,
+		},
+		{
+			headers: response.headers,
+		}
+	);
+};
+
 export default function RootApp() {
+	const { env, session } = useLoaderData<typeof loader>();
+	const fetcher = useFetcher();
+	// it is important to create a single instance of Supabase
+	// to use across client components - outlet context 👇
+	const [supabase] = useState(() => createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_PUBLIC_KEY));
+
+	const serverAccessToken = session?.access_token;
+
+	useEffect(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((event, session) => {
+			if (session?.access_token !== serverAccessToken) {
+				// server and client are out of sync.
+				// Remix recalls active loaders after actions complete
+				fetcher.submit(null, {
+					method: 'post',
+					action: '/?index',
+				});
+			}
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	}, [serverAccessToken, supabase, fetcher]);
+
 	return (
 		<html lang='en'>
 			<head>
@@ -19,7 +90,7 @@ export default function RootApp() {
 				<Links />
 			</head>
 			<body>
-				<Outlet />
+				<Outlet context={{ supabase, session }} />
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
--- app/routes/__auth.tsx
+++ app/routes/__auth.tsx
@@ -1,74 +1,10 @@
-import type { LoaderArgs, LoaderFunction } from '@remix-run/cloudflare';
-import { json } from '@remix-run/cloudflare';
-import { Outlet, useFetcher, useLoaderData } from '@remix-run/react';
-import { createServerClient, createBrowserClient } from '@supabase/auth-helpers-remix';
-import { useState, useEffect } from 'react';
-
-export const loader: LoaderFunction = async ({ request, context }: LoaderArgs) => {
-	// Pipe any necessary environment variables to the client
-	// Cloudflare pages uses the `context` object to pass env vars
-	const env = {
-		SUPABASE_URL: context.SUPABASE_URL,
-		SUPABASE_ANON_PUBLIC_KEY: context.SUPABASE_ANON_PUBLIC_KEY,
-	};
-	// We can retrieve the session on the server and hand it to the client.
-	// This is used to make sure the session is available immediately upon rendering
-	const response = new Response();
-	const supabaseClient = createServerClient(env.SUPABASE_URL as string, env.SUPABASE_ANON_PUBLIC_KEY as string, {
-		request,
-		response,
-	});
-
-	const {
-		data: { session },
-	} = await supabaseClient.auth.getSession();
-
-	// in order for the set-cookie header to be set,
-	// headers must be returned as part of the loader response
-	return json(
-		{
-			env,
-			session,
-		},
-		{
-			headers: response.headers,
-		}
-	);
-};
+import { Outlet } from '@remix-run/react';
 
 export default function __auth() {
-	const { env, session } = useLoaderData<typeof loader>();
-	const fetcher = useFetcher();
-
-	// it is important to create a single instance of Supabase
-	// to use across client components - outlet context 👇
-	const [supabase] = useState(() => createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_PUBLIC_KEY));
-
-	const serverAccessToken = session?.access_token;
-
-	useEffect(() => {
-		const {
-			data: { subscription },
-		} = supabase.auth.onAuthStateChange((event, session) => {
-			if (session?.access_token !== serverAccessToken) {
-				// server and client are out of sync.
-				// Remix recalls active loaders after actions complete
-				fetcher.submit(null, {
-					method: 'post',
-					action: '/?index',
-				});
-			}
-		});
-
-		return () => {
-			subscription.unsubscribe();
-		};
-	}, [serverAccessToken, supabase, fetcher]);
-
 	return (
 		<section className='bg-white dark:bg-gray-900'>
 			<div className='grid lg:h-screen lg:grid-cols-2'>
-				<Outlet context={{ supabase, session }} />
+				<Outlet />
 			</div>
 		</section>
 	);
